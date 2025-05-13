e1f9c51681c96ee6ff648922065dd27bfbf0a547
Saddem labidi <external-slabidi@fr.christianlouboutin.com>
2025-05-13 17:55:09 +0200

    Add logo and sidebar Add Font faces

--- resources/js/Components/Header.tsx
+++ resources/js/Components/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from '@inertiajs/react';
+import ApplicationLogo from '@/Components/ApplicationLogo';
 
 const Header: React.FC = () => {
     return (
         <header className="bg-white text-gray-900">
-            <header className="bg-firefly-950 text-white py-6 px-4 shadow">
+            <header className="bg-firefly-900 text-white py-2 px-4 shadow">
                 <div className="container mx-auto flex items-center justify-between">
-                    <h1 className="text-2xl font-bold">Ocean Portal</h1>
+                    <ApplicationLogo />
                     <nav className="space-x-6">
                         <a href="#about" className="hover:underline">Welcome Back</a>
                     </nav>
