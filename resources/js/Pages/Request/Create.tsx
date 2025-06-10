a2b22dfb38b57b3e288e0b286728bdb607231c14
Saddem labidi <external-slabidi@fr.christianlouboutin.com>
2025-06-10 15:34:54 +0200

    refactor: comment out unused validation rules and clear form errors on submit

--- resources/js/Pages/Request/Create.tsx
+++ resources/js/Pages/Request/Create.tsx
@@ -79,6 +79,7 @@ export default function RequestForm() {
 
 
   const handleSubmitV2 = (mode: 'submit' | 'draft') => {
+    form.clearErrors();
     axios
       .post(
         route(`user.request.submit`, { mode }),
