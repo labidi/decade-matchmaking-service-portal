d1744c89157af590a096d890e5288418f1269758
Saddem Labidi <labidi.saddem@gmail.com>
2025-06-12 03:25:05 +0200

    Handle submit mode in form (#56)
    * fix(opportunity): correct status label retrieval and enhance show page styling (#54)
    
    * skip validation when saving draft

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::middleware(['auth', 'role:user'])->group(function () {
     Route::get('user/request/myrequests', [OcdRequestController::class, 'myRequestsList'])->name('user.request.myrequests');
     Route::get('user/request/edit/{id}', [OcdRequestController::class, 'edit'])->name('user.request.edit');
     Route::get('user/request/show/{id}', [OcdRequestController::class, 'show'])->name('user.request.show');
-    Route::post('user/request/submit/{mode?}', [OcdRequestController::class, 'submit'])->name('user.request.submit');
+    Route::post('user/request/submit', [OcdRequestController::class, 'submit'])->name('user.request.submit');
     Route::patch('user/request/{id}/status', [OcdRequestController::class, 'updateStatus'])->name('user.request.status');
     Route::post('user/request/{request}/document', [\App\Http\Controllers\DocumentController::class, 'store'])->name('user.request.document.store');
     Route::get('user/opportunity/list', [UserOpportunityController::class, 'list'])->name('user.opportunity.list');
