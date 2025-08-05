035f0a1b700d47200b6a57ee3cc18421fead7ba6
Saddem labidi <labidi.saddem@gmail.com>
2025-08-05 17:07:51 +0200

    feat(dropdown): add DropdownActions component for action management

---
+++ resources/js/components/ui/data-table/common/index.ts
@@ -0,0 +1,3 @@
+export { DropdownActions } from '@/components/ui/data-table/common/dropdown-actions';
+export { RequestActions, buildRequestActions } from '@/components/ui/data-table/requests/requests-actions-columns';
+export type { Action } from '@/components/ui/data-table/common/dropdown-actions';
