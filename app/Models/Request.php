272433b3670c70ad86577422ace261f109ef56ae
Saddem labidi <labidi.saddem@gmail.com>
2025-08-21 10:54:36 +0200

    refactor(request): remove duplicate import of BelongsToMany relation

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 
 class Request extends Model
