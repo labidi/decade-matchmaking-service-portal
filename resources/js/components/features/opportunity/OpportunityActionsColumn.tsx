79d59aa0b96b1d406709db103877d9ea25370f27
Saddem labidi <labidi.saddem@gmail.com>
2025-09-12 12:20:11 +0200

    feat (user) : enable and disable home cardlink depends on user roles

--- resources/js/components/features/opportunity/OpportunityActionsColumn.tsx
+++ resources/js/components/features/opportunity/OpportunityActionsColumn.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {DropdownActions} from '@/components/ui/data-table/common/dropdown-actions';
-import {Opportunity} from '@/types';
+import {Opportunity, Context} from '@/types';
 import {useOpportunityActions} from '@/hooks/useOpportunityActions';
 
 interface OpportunityActionsProps {
     opportunity: Opportunity;
-    context: string,
+    context: Context,
     showRouteName: string
 }
 
@@ -14,7 +14,7 @@ export function OpportunityActions({opportunity, context, showRouteName}: Readon
         getActionsForOpportunity,
     } = useOpportunityActions(context, showRouteName);
 
-    const actions = getActionsForOpportunity(opportunity);
+    const actions = getActionsForOpportunity(context, opportunity);
 
     return (
         <DropdownActions actions={actions}/>
