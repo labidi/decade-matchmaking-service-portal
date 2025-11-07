2b41a77565b8647d23a9ca108d55585daa3cfb96
Saddem labidi <labidi.saddem@gmail.com>
2025-11-07 13:59:47 +0100

    app : add site icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="icon" type="image/x-icon" href={{asset('assets/img/cropped-ocean-decade-icone-2-32x32.png')}}>
         <title inertia>{{ config('app.name') }}</title>
 
         <!-- Scripts -->
