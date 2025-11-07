4336350468d71d193dd3f6b48643fb476242bd4c
Saddem labidi <labidi.saddem@gmail.com>
2025-11-07 14:59:25 +0100

    Opportunity : fix breadcrumbs

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -4,7 +4,42 @@ use Diglactic\Breadcrumbs\Breadcrumbs;
 use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
 
 
-Breadcrumbs::for('user.home', function (BreadcrumbTrail $trail) {
-    $trail->push('Home', route('user.home'));
+try {
+    Breadcrumbs::for('opportunity.create', function (BreadcrumbTrail $trail) {
+        $trail->parent('user.home');
+        $trail->push('Submit a new Opportunity', route('opportunity.create'));
+    });
+    Breadcrumbs::for('user.home', function (BreadcrumbTrail $trail) {
+        $trail->push('Home', route('user.home'));
+    });
+    Breadcrumbs::for('me.opportunity.list', function (BreadcrumbTrail $trail) {
+        $trail->parent('user.home');
+        $trail->push('My submitted opportunities', route('me.opportunity.list'));
+    });
+    Breadcrumbs::for('opportunity.list', function (BreadcrumbTrail $trail) {
+        $trail->parent('user.home');
+        $trail->push('View and Apply for Partner Opportunities', route('opportunity.list'));
+    });
+    Breadcrumbs::for('me.opportunity.show', function (BreadcrumbTrail $trail) {
+        $trail->parent('user.home');
+        $trail->push('My submitted opportunities', route('me.opportunity.list'));
+        $trail->push('My Opportunity details', route('me.opportunity.show', request('id')));
+    });
+} catch (\Diglactic\Breadcrumbs\Exceptions\DuplicateBreadcrumbException $e) {
+
+}
+
+
+
+
+Breadcrumbs::for('opportunity.show', function (BreadcrumbTrail $trail) {
+    $trail->parent('user.home');
+    $trail->push('View and Apply for Partner Opportunities', route('opportunity.list'));
+    $trail->push('Opportunity details', route('opportunity.show', request('id')));
 });
 
+Breadcrumbs::for('opportunity.edit', function (BreadcrumbTrail $trail) {
+    $trail->parent('user.home');
+    $trail->push('My submitted opportunities', route('me.opportunity.list'));
+    $trail->push('Edit Opportunity', route('opportunity.edit', request('id')));
+});
--- routes/opportunity.php
+++ routes/opportunity.php
@@ -38,37 +38,3 @@ Route::middleware(['auth', 'role:administrator'])->prefix('admin')->group(functi
     Route::get('opportunity/list', ListController::class)->name('admin.opportunity.list');
     Route::get('opportunity/{id}/show', ShowController::class)->name('admin.opportunity.show');
 });
-
-
-Breadcrumbs::for('opportunity.create', function (BreadcrumbTrail $trail) {
-    $trail->parent('user.home');
-    $trail->push('Submit a new Opportunity', route('opportunity.create'));
-});
-
-Breadcrumbs::for('me.opportunity.list', function (BreadcrumbTrail $trail) {
-    $trail->parent('user.home');
-    $trail->push('My submitted opportunities', route('me.opportunity.list'));
-});
-
-Breadcrumbs::for('opportunity.list', function (BreadcrumbTrail $trail) {
-    $trail->parent('user.home');
-    $trail->push('View and Apply for Partner Opportunities', route('opportunity.list'));
-});
-
-Breadcrumbs::for('me.opportunity.show', function (BreadcrumbTrail $trail) {
-    $trail->parent('user.home');
-    $trail->push('My submitted opportunities', route('me.opportunity.list'));
-    $trail->push('My Opportunity details', route('me.opportunity.show', request('id')));
-});
-
-Breadcrumbs::for('opportunity.show', function (BreadcrumbTrail $trail) {
-    $trail->parent('user.home');
-    $trail->push('View and Apply for Partner Opportunities', route('opportunity.list'));
-    $trail->push('Opportunity details', route('opportunity.show', request('id')));
-});
-
-Breadcrumbs::for('opportunity.edit', function (BreadcrumbTrail $trail) {
-    $trail->parent('user.home');
-    $trail->push('My submitted opportunities', route('me.opportunity.list'));
-    $trail->push('Edit Opportunity', route('opportunity.edit', request('id')));
-});
